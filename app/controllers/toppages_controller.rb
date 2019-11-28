872f300ef7724eab240c47a9193f4b5cf3e5f308
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-28 13:49:18 +0900

    Update toppages_controller.rb

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,9 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @user = current_user
+      @tweet = current_user.tweets.build  # form_for 用
+      @tweets = current_user.tweets.order('created_at DESC').page(params[:page])
+    end
   end
 end
