85d8cbecbc8a7d06d5f6119c2a801fa30f6b0943
ryoma-desuyo <relicsjcbass4@gmail.com>
2019-11-28 14:22:14 +0900

    Update toppages_controller.rb

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,8 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @user = current_user
-      @tweet = current_user.tweets.build  # form_for 用
-      @tweets = current_user.tweets.order('created_at DESC').page(params[:page])
+      @tweet = current_user.tweets.build
+      @tweets = Tweet.where(user_id: current_user.following_ids + [current_user.id]).order('created_at DESC').page(params[:page])
     end
   end
 end
